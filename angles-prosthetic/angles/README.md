97fe17d9d81350e8650270ef1af06a6f7d0ff750
Graham O'Regan <graham.oregan@ellisonbrookes.com>
2012-05-11 11:01:25 +0100

    adding first cut of PTK

---
+++ angles-prosthetic/angles/README.md
@@ -0,0 +1,4 @@
+# blank
+
+Needs some loving
+
---
+++ angles-prosthetic/angles/admin.py
@@ -0,0 +1,43 @@
+# Copyright (C) 2011 Philter Phactory Ltd.
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE X
+# CONSORTIUM BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# 
+# Except as contained in this notice, the name of Philter Phactory Ltd. shall
+# not be used in advertising or otherwise to promote the sale, use or other
+# dealings in this Software without prior written authorization from Philter
+# Phactory Ltd..
+#
+
+
+from django.contrib import admin
+import djangotoolbox.fields as toolbox
+from django import forms
+from django.forms import ModelForm
+from django.shortcuts import get_object_or_404, render_to_response, redirect
+from django.db import models
+from django.contrib import messages
+from django.utils.html import escape
+
+try: from django.utils import simplejson as json
+except ImportError: import json
+
+import datetime
+
+from models import *
+
+
